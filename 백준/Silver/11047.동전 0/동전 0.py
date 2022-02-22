3a3dfba12f9217a06b70a8b345991111ab05b7fa
Chanmi Yoo <55586110+chanmeee@users.noreply.github.com>
2022-02-22 10:45:53 +0900

    [Silver III] Title: 동전 0, Time: 68 ms, Memory: 30864 KB -BaekjoonHub

--- 백준/Silver/11047.동전 0/동전 0.py
+++ 백준/Silver/11047.동전 0/동전 0.py
@@ -1,9 +1,12 @@
-N, K = map(int, input().split()) 
+import sys
+
+N, K = map(int, sys.stdin.readline().split())  # N: 동전 종류, K : 가치의 합
+
 coin_lst = list()
 for i in range(N):
-    coin_lst.append(int(input()))
+    coin_lst.append(int(sys.stdin.readline()))
 
-count = 0
+count = 0 
 for i in reversed(range(N)):
     count += K//coin_lst[i] #카운트 값에 K를 동전으로 나눈 몫을 더해줌
     K = K%coin_lst[i] # K는 동전으로 나눈 나머지로 계속 반복
