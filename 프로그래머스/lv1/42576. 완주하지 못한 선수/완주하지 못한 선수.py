5fbf0e8aa5f258e25d6a6f7797e18c84ed4c3bcd
Chanmi Yoo <55586110+chanmeee@users.noreply.github.com>
2023-03-10 02:11:28 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 53.37 ms, Memory: 37.7 MB -BaekjoonHub

--- 프로그래머스/lv1/42576. 완주하지 못한 선수/완주하지 못한 선수.py
+++ 프로그래머스/lv1/42576. 완주하지 못한 선수/완주하지 못한 선수.py
@@ -2,10 +2,38 @@
 # completion : 완주 선수 이름 
 # 완주하지 못한 선수의 이름 반환 
 
-from collections import Counter 
-def solution(participant, completion):
+# from collections import Counter 
+# def solution(participant, completion):
 
-    diff = Counter(participant) - Counter(completion)
+#     diff = Counter(participant) - Counter(completion)
   
-    answer = list(diff)[0]
+#     answer = list(diff)[0]
+#     return answer 
+
+# def solution(participant, completion):
+    
+#     answer = ''
+    
+#     for idx in range(len(participant) - 1):
+#         if completion[idx] != participant[idx]:
+#             answer = participant[idx] 
+    
+#     return answer 
+
+
+
+# 다른 사람 풀이 
+def solution(participant, completion):
+    
+    answer = ''
+    temp = 0
+    dic = {}
+    
+    for part in participant:
+        dic[hash(part)] = part
+        temp += int(hash(part))
+    for com in completion:
+        temp -= hash(com)
+    answer = dic[temp]
+
     return answer 
