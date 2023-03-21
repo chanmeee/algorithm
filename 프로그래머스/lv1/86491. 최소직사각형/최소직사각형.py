8e094b7d83a76e0a61ba17bf8830b1623a098786
Chanmi Yoo <55586110+chanmeee@users.noreply.github.com>
2023-03-22 02:09:17 +0900

    [level 1] Title: 최소직사각형, Time: 4.56 ms, Memory: 11.7 MB -BaekjoonHub

--- 프로그래머스/lv1/86491. 최소직사각형/최소직사각형.py
+++ 프로그래머스/lv1/86491. 최소직사각형/최소직사각형.py
@@ -1,13 +1,12 @@
 def solution(sizes):
     answer = 0
+    w_list=[]
+    h_list=[]
     
-    max_list = []
-    min_list = []
-
-    for s in sizes:
-        max_list.append(max(s))
-        min_list.append(min(s))
-
-    answer = max(max_list) * max(min_list)
+    for i in range(len(sizes)):
+        w_list.append(max(sizes[i])) 
+        h_list.append(min(sizes[i]))
+        
+    answer = max(w_list) * max(h_list)
     
     return answer
