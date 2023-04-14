8bfbcd5105a10e6c9aae1cbca8cfe0f7af185802
Chanmi Yoo <55586110+chanmeee@users.noreply.github.com>
2023-04-15 08:57:53 +0900

    [level 1] Title: 모의고사, Time: 2.05 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv1/42840. 모의고사/모의고사.py
+++ 프로그래머스/lv1/42840. 모의고사/모의고사.py
@@ -1,29 +1,22 @@
 def solution(answers):
     answer = []
+    p1=[1,2,3,4,5] # -> [0,1,2,3,4] 
+    p2=[2, 1, 2, 3, 2, 4, 2, 5]
+    p3=[3, 3, 1, 1, 2, 2, 4, 4, 5, 5] 
+    score=[0,0,0]
     
-    # 패턴 입력 
-    pattern1 = [1,2,3,4,5]  # 5
-    pattern2 = [2,1,2,3,2,4,2,5]  # 8
-    pattern3 = [3,3,1,1,2,2,4,4,5,5]  # 10
+    for i, a in enumerate(answers) :
+        # 수포자1 찍은 답안=정답이면,
+        if p1[i % 5] == a: 
+            score[0] +=1
+        # 수포자2 
+        if p2[i % 8] == a:
+            score[1] +=1
+        # 수포자3 
+        if p3[i % 10] == a:
+            score[2] +=1
+            
+    max_value = max(score) 
+    answer=[i+1 for i, s in enumerate(score) if s==max_value] 
     
-    # 수포자별 정답 개수 세기 -> 나머지를 이용!  
-    corr_num1, corr_num2, corr_num3 = 0, 0, 0
-    for idx in range(len(answers)):
-        
-        if pattern1[idx % 5] == answers[idx]:
-            corr_num1 += 1
-        if pattern2[idx % 8] == answers[idx]:
-            corr_num2 += 1
-        if pattern3[idx % 10] == answers[idx]:
-            corr_num3 += 1
-    
-    # 최대값 저장 후, 최대값이면 정답에 append 
-    max_num = max(corr_num1, corr_num2, corr_num3)
-    if max_num == corr_num1:
-        answer.append(1)
-    if max_num == corr_num2:
-        answer.append(2)
-    if max_num == corr_num3:
-        answer.append(3) 
-        
-    return answer
+    return answer 
