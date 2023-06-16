e4a2b7131df88dbc05415a8f31f1ecb59f687d2f
Chanmi Yoo <55586110+chanmeee@users.noreply.github.com>
2023-06-16 23:58:55 +0900

    [Silver II] Title: 특정 거리의 도시 찾기, Time: 1744 ms, Memory: 101272 KB -BaekjoonHub

--- 백준/Silver/18352. 특정 거리의 도시 찾기/특정 거리의 도시 찾기.py
+++ 백준/Silver/18352. 특정 거리의 도시 찾기/특정 거리의 도시 찾기.py
@@ -1,5 +1,9 @@
-from collections import deque
-import sys
+# DFS/BFS # ex) 15 
+
+## 모든 도로의 거리는 1 = 모든 간선의 비용이 1 => bfs 이용
+
+import sys 
+from collections import deque 
 f = sys.stdin.readline
 
 n, m, k, x = map(int, f().split())
@@ -7,9 +11,43 @@ graph = [[] for _ in range(n+1)]
 distance = [0] * (n+1)
 visited = [False] * (n+1)
 
+# 도로 정보 입력받기 
 for _ in range(m):
     a, b = map(int, f().split())
     graph[a].append(b)
+
+# bfs로 탐색
+def bfs(start):
+    answer = []
+    q = deque([start])
+    visited[start] = True
+    distance[start] = 0
+
+    while q:
+        # 도시를 popleft 
+        now = q.popleft()
+        
+        # 현재 도시에서 이동할 수 있는 모든 도시를 확인 
+        for next_node in graph[now]:
+            # 아직 방문 안했다면, 방문처리 & 큐에 추가 & 최단거리 업데이트
+            if not visited[next_node]:
+                visited[next_node] = True 
+                q.append(next_node)
+                distance[next_node] = distance[now] +1
+                # next 도시의 거리 값이 K이면, answer에 추가 
+                if distance[next_node] == k:
+                    answer.append(next_node)
+        
+        # 최단거리가 k인 도시가 하나도 없으면, -1 출력
+        if len(answer) == 0:
+            print(-1) 
+        
+        # 최단거리가 K인 도시가 있으면, 오름차순으로 출력
+        else:
+            answer.sort()
+            for i in answer:
+                print(i, end='\n')
+                
 
 def bfs(start):
     answer = []
@@ -31,5 +69,5 @@ def bfs(start):
         answer.sort()
         for i in answer:
             print(i, end='\n')
-
+            
 bfs(x)
