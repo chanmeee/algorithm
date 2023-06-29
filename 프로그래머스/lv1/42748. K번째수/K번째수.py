3ec0bed40f0f90b7bc45dc3a7fbefe5c370be44a
Chanmi Yoo <55586110+chanmeee@users.noreply.github.com>
2023-06-29 20:54:58 +0900

    [level 1] Title: K번째수, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv1/42748. K번째수/K번째수.py
+++ 프로그래머스/lv1/42748. K번째수/K번째수.py
@@ -1,11 +1,17 @@
 def solution(array, commands):
     answer = []
     
-    for i, j, k in commands:
+    for c in commands:
         
-        sorted_l = sorted(array[i-1:j])
-        answer.append(sorted_l[k-1])
-    
-    
+#         # 자르기 
+#         print(array[(c[0]-1):c[1]])
+        
+#         # 정렬 
+#         print(sorted(array[(c[0]-1):c[1]]) )
+
+#         # 인덱싱 
+#         print(sorted(array[(c[0]-1):c[1]])[c[2]-1]) 
+        
+        answer.append( sorted(array[(c[0]-1):c[1]])[c[2]-1] )
     
     return answer
