f3877418bfc9b60b2e714994eaaecfb24c4495a6
Chanmi Yoo <55586110+chanmeee@users.noreply.github.com>
2023-06-30 14:17:19 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 42.88 ms, Memory: 33.9 MB -BaekjoonHub

--- 프로그래머스/lv1/42576. 완주하지 못한 선수/완주하지 못한 선수.py
+++ 프로그래머스/lv1/42576. 완주하지 못한 선수/완주하지 못한 선수.py
@@ -1,7 +1,24 @@
 def solution(participant, completion):
     answer = ''
-    from collections import Counter
-
-    answer=list((Counter(participant) - Counter(completion)).elements() ).pop()
-
-    return answer
+    dict1 = {}  # participant 
+    #dict_c = {}  # completion 
+    
+    for p in participant:
+        if p in dict1:
+            dict1[p] += 1
+        else:
+            dict1[p] = 1
+    for c in completion:
+        if c in dict1:
+            dict1[c] -= 1
+        else:
+            dict1[c] = 1
+                        
+    #print(dict1)
+    for k,v in dict1.items():
+        if v !=0:
+            return k
+    
+    
+    
+    #return answer
