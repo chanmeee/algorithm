4a7ac6d5d801d736ab9e75b1ee25f44b19fb7dac
Chanmi Yoo <55586110+chanmeee@users.noreply.github.com>
2023-06-30 14:30:05 +0900

    [level 2] Title: 가장 큰 수, Time: 1.89 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/lv2/42746. 가장 큰 수/가장 큰 수.py
+++ 프로그래머스/lv2/42746. 가장 큰 수/가장 큰 수.py
@@ -1,13 +1,20 @@
 def solution(numbers):
     answer = ''
     
-    # 1) 문자열로 변환 
-    numbers = map(str, numbers)
+    # 1) 문자열 변환
+    numbers = map(str, numbers) 
     
-    # 2) 사전 값으로 정렬  ex) 9부터 1까지
-    numbers = sorted(numbers, key= lambda num :num*3,reverse=True)  # 단, 숫자*3 해줌 (30보다 3이 먼저 와야하므로)
+    # 2) 알파벳순 정렬
+    numbers = sorted(numbers, key=lambda x: x*3, reverse=True)
+    print(numbers)  
     
     # 3) 붙이기 
-    answer = str(int(''.join(numbers)))  # 0000 -> 0 나오도록 str을 int로 바꿨다가 다시 str로 변환 
+    answer = str(int(''.join(numbers)))
     
     return answer
+
+
+def solution(numbers):
+    numbers = list(map(str, numbers))
+    numbers.sort(key=lambda x: x*3, reverse=True)
+    return str(int(''.join(numbers)))
