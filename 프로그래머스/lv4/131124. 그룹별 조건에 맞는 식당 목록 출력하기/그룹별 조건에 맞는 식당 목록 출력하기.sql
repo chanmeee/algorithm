47a5d4e017344526b9e26dbb2938c9a9eba84fb1
Chanmi Yoo <55586110+chanmeee@users.noreply.github.com>
2023-07-05 23:39:17 +0900

    [level 4] Title: 그룹별 조건에 맞는 식당 목록 출력하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/lv4/131124. 그룹별 조건에 맞는 식당 목록 출력하기/그룹별 조건에 맞는 식당 목록 출력하기.sql
+++ 프로그래머스/lv4/131124. 그룹별 조건에 맞는 식당 목록 출력하기/그룹별 조건에 맞는 식당 목록 출력하기.sql
@@ -1,20 +1,21 @@
--- 리뷰를 가장 많이 작성한 회원 
--- 리뷰들 조회 
--- 리뷰 작성일 기준 오름차순, 리뷰 텍스트 기준 오름차순
+-- 리뷰를 가장 많이 작성한 회원
+-- 리뷰 작성일 기준 오름차순, 리뷰 텍스트를 기준 오름차순
 
-# 1) 리뷰 가장 많은 회원 찾기 -- 임시테이블 
-# 2) 1테이블 기준 INNER JOIN 
-WITH best_reviewer AS(
-    SELECT MEMBER_ID, count(REVIEW_ID) as CNT
-    FROM REST_REVIEW 
-    GROUP BY MEMBER_ID 
-    ORDER BY CNT desc
+WITH most_reviews AS(
+    SELECT member_id,
+            count(*) AS reviews
+    FROM rest_review  r
+    GROUP BY member_id
+    ORDER BY reviews desc
     LIMIT 1 
 )
 
-SELECT p.MEMBER_NAME, r.REVIEW_TEXT, 
-        date_format(r.REVIEW_DATE, '%Y-%m-%d') as REVIEW_DATE
-FROM best_reviewer br
-INNER JOIN MEMBER_PROFILE p ON br.MEMBER_ID = p.MEMBER_ID
-INNER JOIN REST_REVIEW r ON r.MEMBER_ID = p.MEMBER_ID
+SELECT m.MEMBER_NAME, 
+			r.REVIEW_TEXT,
+	    date_format(r.REVIEW_DATE,'%Y-%m-%d') as REVIEW_DATE
+    
+FROM most_reviews as mr
+INNER JOIN MEMBER_PROFILE as m  ON mr.member_id = m.member_id
+INNER JOIN REST_REVIEW as r  ON m.member_id = r.member_id
+
 ORDER BY REVIEW_DATE, REVIEW_TEXT
