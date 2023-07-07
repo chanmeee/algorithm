dcf8e24001171b403a2fa24a27035acdf0930175
Chanmi Yoo <55586110+chanmeee@users.noreply.github.com>
2023-07-07 11:21:39 +0900

    [level 2] Title: 올바른 괄호, Time: 14.23 ms, Memory: 10.8 MB -BaekjoonHub

--- 프로그래머스/lv2/12909. 올바른 괄호/올바른 괄호.py
+++ 프로그래머스/lv2/12909. 올바른 괄호/올바른 괄호.py
@@ -1,21 +1,27 @@
 def solution(s):
+    
+    from collections  import deque 
+    que = deque(s)   # leftpop() 쓰려면 deque여야 함 list에는 적용 불가능 
+    stack = [] 
+    answer = True 
+    
+    while que:  # que가 비어있지 않으면 
+        temp = que.popleft() 
         
-    stack = []
+        # 열린 괄호이면 
+        if temp == '(':
+            stack.append(temp)
+        
+        # 닫힌 괄호이면 
+        elif temp == ')' and stack and stack[-1] == '(':
+            stack.pop() 
+        else: 
+            return False 
     
-    for i in s : 
-        # 여는 괄호이면, stack에 쌓기 
-        if i == '(': 
-            stack.append(i) 
-        # 닫는 괄호이면, stack 맨 오른쪽 확인 후 제거 
-        elif i == ')' and stack==[]:
-            stack.append(i)  
-        elif i == ')' and stack[-1] == '(':
-            stack.pop(-1) 
-    
-    if stack != []: 
-        return False  
-    else: 
+    # stack에 남아있는 괄호가 있으면 (다 처리안된 경우)
+    if stack : 
+        return False 
+    else:
         return True 
     
-    # 닫힌 괄호 나오면, stack의 가장 최근(오른쪽)만 확인 -> 쌍이 맞나? 
-    # 맞으면 -> stack에서 해당 요소 제거
+    
