a2be2f3d0ee69bd260d42379207242f47dc59866
Chanmi Yoo <55586110+chanmeee@users.noreply.github.com>
2023-09-06 01:54:48 +0900

    [level 1] Title: 폰켓몬, Time: 0.82 ms, Memory: 10.5 MB -BaekjoonHub

--- 프로그래머스/lv1/1845. 폰켓몬/폰켓몬.py
+++ 프로그래머스/lv1/1845. 폰켓몬/폰켓몬.py
@@ -1,19 +1,10 @@
-# nums: N마리 폰켓몬의 종류 번호가 담긴 배열 
-# 총 N 마리의 폰켓몬 중에서 N/2마리를 가져가도 좋다  
+from collections import Counter
 
 def solution(nums):
-    
     answer = 0
-    N = len(nums)
+    n = len(nums) 
+    nums_c = Counter(nums) 
+    answer = len(nums_c) 
     
-    # N/2 <= 포켓몬 가짓수 
-    if N/2 <= len(set(nums)) :
-        answer = N/2
-        
-    # N/2 > 포켓몬 가짓수
-    elif N/2 > len(set(nums)) :
-        answer = len(set(nums)) 
-
-    answer = int(answer)
-    return answer
     
+    return n/2 if n/2 < answer else answer
